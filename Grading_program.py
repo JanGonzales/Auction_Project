ef3fecc67a6d9e6238cda6e641c2078b6a368c92
Jan Gonzales <JanGonFlores@icloud.com>
2021-01-28 22:10:46 +0000

    ,

--- Grading_program.py
+++ Grading_program.py
@@ -15,7 +15,7 @@ for scores in student_scores:
     if student_scores[scores] >= 91:
         student_grades[scores] = "Outstanding"
     elif student_scores[scores] >= 81:
-        student_grades[scores] = "Exceed Expectations"
+        student_grades[scores] = "Exceeds Expectations"
     elif student_scores[scores] >= 71:
         student_grades[scores] = "Acceptable"
     else:
